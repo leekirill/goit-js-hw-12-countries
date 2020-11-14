d8053cb748c42503a0795208e3eaf4387892aae4
Kirill Lee <cyril.lee1997@gmail.com>
2020-11-14 19:19:11 +0200

    update

--- js/fetchCountries.js
+++ js/fetchCountries.js
@@ -3,13 +3,10 @@ const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 function fetchCountriesByName(name) {
     return fetch(`${BASE_URL}/${name}`).then(function (response) {
         if (!response.ok) {
-            throw new Error('Not 2xx response');
-        } else {
-            return response.json();
+            throw new Error();
         }
+        return response.json();
     });
 }
 
 export default { fetchCountriesByName };
-
-console.log(fetchCountriesByName());
